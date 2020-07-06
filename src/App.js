55f0e1eef7899b2786cdbb06af4737783c41e65f
yulepetrilli <67811244+yulepetrilli@users.noreply.github.com>
2020-07-06 11:16:42 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  state = {
+    checked: localStorage.getItem("theme") === "dark"? true : false,
+    theme: localStorage.getItem("theme")
+  };
+  componentDidMount(){
+    document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+  }
+  toggleThemeChange = () =>{
+    const {checked} = this.state;
+
+    if(checked === false){
+      localStorage.setItem("theme", "dark");
+      document
+          .getElementsByTagName("HTML")[0]
+          .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: true
+      });
+    }else{
+      localStorage.setItem("theme", "light");
+      document
+          .getElementsByTagName("HTML")[0]
+          .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: false
+      });
+    }
+  };
+
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Click the switch to toggle themes
+          </p>
+          <br/>
+          <label className = "switch">
+              <input type = "checkbox" 
+              defaultChecked = {this.state.checked} onChange={() => this.toggleThemeChange()}/>
+              <span className = "slider round"/>
+          </label>
+        </header>
+      </div>
+    );  
+  }
+}
+
+export default App;
